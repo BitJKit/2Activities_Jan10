3367fc0bb7cbd921f549a4d847ca2b4ef67b77d6
BitJKit <ionathan.m.kelly@gmail.com>
2022-01-11 11:43:44 -0500

    SecondActivity toast init

--- app/src/main/java/com/example/a2activities_jan10/SecondActivity.kt
+++ app/src/main/java/com/example/a2activities_jan10/SecondActivity.kt
@@ -2,6 +2,7 @@ package com.example.a2activities_jan10
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity: AppCompatActivity() {
@@ -14,6 +15,7 @@ class SecondActivity: AppCompatActivity() {
 
         mainBtn = findViewById(R.id.btnMain)
         mainBtn.text = getString(R.string.second_screen)
+        Toast.makeText(applicationContext, "Hello World!", Toast.LENGTH_LONG).show()
     }
 
 }
